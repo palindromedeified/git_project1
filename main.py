46ebe801a2f3fcb484aed3a589a4cf57e98e4240
andrey <holanpolain090@gmail.com>
2024-01-12 11:15:45 +0300

    New day — new print

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     print('My first git program')
+    print('And I change it every day')
 
 
 if __name__ == '__main__':
     main()
 
 # Чтобы система сохранила текущую версию программы, ей нужно дать сигнал через коммит. Команда Git → commit (Ctrl + K)
+# Отменить изменения — Git → Uncommited Changes → Rollback. Эта команда откатит файл к последней зафиксированной версии.
+# Зафиксировать изменения — Git → Commit

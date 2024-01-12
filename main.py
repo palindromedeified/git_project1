92b8d18b70217dfdf9f737f589df4acbb048a9eb
andrey <holanpolain090@gmail.com>
2024-01-12 11:07:13 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print('My first git program')
+
+
+if __name__ == '__main__':
+    main()
+
+# Чтобы система сохранила текущую версию программы, ей нужно дать сигнал через коммит. Команда Git → commit (Ctrl + K)
